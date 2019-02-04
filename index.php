65ff51574d087a8648b73b633269a1295184318a
D00202405 <D00202405@P1105-PC14.ad.dkit.ie>
2019-02-04 16:28:40 +0000

    First Commit, containing base code

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php include 'view/header.php'; ?>
+<link href="main.css" rel="stylesheet" type="text/css"/>
+<main>
+    <h1>Menu</h1>
+    <ul>
+        <li>
+            <a href="player_manager">Product Manager</a>
+        </li>
+        <li>
+            <a href="teams">Product Catalog</a>
+        </li>
+    </ul>
+</main>
+<?php include 'view/footer.php'; ?>
