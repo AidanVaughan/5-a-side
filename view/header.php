dd68f6b23569790e7a7971d71c991f2d9054598a
D00202405 <D00202405@P1105-PC14.ad.dkit.ie>
2019-02-18 16:26:20 +0000

    -Added headers and footers based on includes.

--- view/header.php
+++ view/header.php
@@ -2,11 +2,11 @@
 <html>
 <!-- the head section -->
 <head>
-    <title>My Guitar Shop</title>
+    <title>5-a-side Management</title>
     <link href="../main.css" rel="stylesheet" type="text/css"/>
 </head>
 
 <!-- the body section -->
 <body>
-<header><h1>My Guitar Shop</h1></header>
+<header><h1>5-a-side Management</h1></header>
        
